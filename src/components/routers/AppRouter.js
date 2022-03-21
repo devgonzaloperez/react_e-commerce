4af7a1570a6d5143a8b6e75b5f165bd4ddbb85ba
devgonzaloperez <gonzalonahuelperez@gmail.com>
2022-03-21 11:49:08 +0100

    project completed

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -4,8 +4,6 @@ import { HomeScreen } from "../home/HomeScreen";
 import { CatalogScreen } from "../catalog/CatalogScreen";
 import { CartScreen } from "../cart/CartScreen";
 import { ItemDetailContainer } from "../catalog/ItemDetailContainer";
-import { AddItem } from "../admin/AddItem";
-import { EditItem } from "../admin/EditItem";
 
 export const AppRouter = () => {
     return (
@@ -17,8 +15,6 @@ export const AppRouter = () => {
                     <Route exact path="/catalog/category/:category" element={<CatalogScreen/>}/>
                     <Route exact path="/catalog/product/:itemID" element={<ItemDetailContainer/>}/>
                     <Route exact path="/cart" element={<CartScreen/>}/>
-                    <Route exact path="/admin/additem" element={<AddItem/>}/>
-                    <Route exact path="/admin/edititem/:id" element={<EditItem/>}/>
                     <Route path="*" element={<Navigate to="/"/>}/>
                 </Routes>
         </Router>
